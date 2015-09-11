a836c0f0d93a9dfe886dc0b8eff67f60bfed9ffe
Denis Dovganich <t1mmmmY111@gmail.com>
2015-09-11 11:58:12 +0200

    Gridview

---
+++ Assets/SlotMachine/Scripts/VectrosityGridview.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Vectrosity;
+
+[RequireComponent(typeof(UIPanel))]
+public class VectrosityGridview : MonoBehaviour 
+{
+	[SerializeField] UICamera camera;
+	[SerializeField] Rect testRect;
+
+	UIPanel panel;
+	VectorLine line;
+	
+	void Start ()
+	{
+		panel = GetComponent<UIPanel>();
+		line = new VectorLine("Grid", new List<Vector3>(), null, 3f, LineType.Discrete, Joins.Weld);	
+		VectorLine.SetCanvasCamera(camera.cachedCamera);
+		VectorLine.canvas.planeDistance = 0;
+		VectorLine.canvas.gameObject.layer = panel.gameObject.layer;
+		DrawRect();
+	}
+
+	void DrawRect()
+	{
+		line.MakeRect(testRect);
+	}
+}
